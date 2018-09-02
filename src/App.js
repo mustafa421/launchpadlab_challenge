77ae6162d235b2349b90402313f4321eabbed983
Mustafa Abdelhamid <abdelha5@uwm.edu>
2018-09-01 23:09:38 -0500

    setting up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Card from "@material-ui/core/Card";
+import { CardContent } from "../node_modules/@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 import "./App.css";
+
+const frameworks = ["React", "Angular", "Ember", "Vue"];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card className={{ minWidth: 275, height: 140 }}>
+          <CardContent>
+            {frameworks.map(framework => (
+              <Typography variant="title" gutterBottom>
+                {framework}
+              </Typography>
+            ))}
+          </CardContent>
+        </Card>
       </div>
     );
   }
